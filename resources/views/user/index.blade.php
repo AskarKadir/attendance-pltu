cc745ad90abeec0baec705622ca09fdffda3808f
AskarKadir <askarkadir07@gmail.com>
2023-08-17 18:33:04 +0700

    view fix

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -53,21 +53,75 @@
                                     Email
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    Status
-                                </th>
-                                <th scope="col" class="px-6 py-3">
                                     Action
                                 </th>
                             </tr>
                         </thead>
+                        <tbody>
+                            @forelse ($users as $user)
+                                <tr class="odd:bg-white odd:dark:bg-gray-800 even:bg-gray-50 even:dark:bg-gray-700">
+                                    <td scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <p>{{ $user->id }}</p>
+                                    </td>
+                                    <td
+                                        class="px-6 py-4 font-medium text-gray-900 md:whitespace-nowrap dark:text-white">
+                                        <p>{{ $user->name }}</p>
+                                    </td>
+                                    <td class="hidden px-6 py-4 md:block">
+                                        <p>{{ $user->email }}</p>
+                                    </td>
+
+                                    <td class="px-6 py-4">
+                                        <div class="flex space-x-3">
+                                            {{-- Action here --}}
+                                            @if ($user->is_admin)
+                                                <form action="{{ route('user.removeadmin', $user) }}" method="Post">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <button type="submit"
+                                                        class="text-blue-600 dark:text-blue-400 whitespace-nowrap">
+                                                        Remove Admin
+                                                    </button>
+                                                </form>
+                                            @else
+                                                <form action="{{ route('user.makeadmin', $user) }}" method="Post">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <button type="submit"
+                                                        class="text-red-600 dark:text-red-400 whitespace-nowrap">
+                                                        Make Admin
+                                                    </button>
+                                                </form>
+                                            @endif
+                                            <form action="{{ route('user.destroy', $user) }}" method="Post">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit"
+                                                    class="text-red-600 dark:text-red-400 whitespace-nowrap">
+                                                    Delete
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </td>
+
+                                </tr>
+                            @empty
+                                <tr class="bg-white dark:bg-gray-800">
+                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                        Empty
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
 
                     </table>
                 </div>
-                {{-- @if ($users->hasPages())
+                @if ($users->hasPages())
                     <div class="p-6">
                         {{ $users->links() }}
                     </div>
-                @endif --}}
+                @endif
             </div>
         </div>
     </div>
