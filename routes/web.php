de2f6eb175828bf721d51d464e951a9cc9e853e7
AskarKadir <askarkadir07@gmail.com>
2023-08-17 11:59:05 +0700

    simple route

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,15 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/attendance', function () {
+        return view('attendance/index');
+    })->name('attendance');
+    Route::get('/user', function () {
+        return view('user/index');
+    })->name('user');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
