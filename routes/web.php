84af70d451a2eb187ae96b9e8b6f3e945c5ce4ad
AskarKadir <askarkadir07@gmail.com>
2023-08-21 16:18:56 +0700

    route management for user

--- routes/web.php
+++ routes/web.php
@@ -32,13 +32,17 @@ Route::middleware('auth')->group(function () {
     Route::patch('attendance/{attendance}/absen', [AttendanceController::class, 'absen'])->name('attendance.absen');
     Route::patch('attendance/{attendance}/sakit', [AttendanceController::class, 'sakit'])->name('attendance.sakit');
     Route::patch('attendance/{attendance}/izin', [AttendanceController::class, 'izin'])->name('attendance.izin');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+});
+
+Route::middleware('admin')->group(function () {
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
     Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
