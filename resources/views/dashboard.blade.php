448f8dc33b07b65e78f29f5a08c44514c5f9a856
AskarKadir <askarkadir07@gmail.com>
2023-08-21 16:40:19 +0700

    hide datetime summary

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,18 +13,18 @@
                 </div>
             </div>
         </div>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 my-5">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100 text-center">
-                    {{ __('Summary Attendance Status For') }}
-                    <?php
-                    $currentDateTime = date('l, d F Y');
-                    echo $currentDateTime;
-                    ?>
+        @can('admin')
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 my-5">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100 text-center">
+                        {{ __('Summary Attendance Status For') }}
+                        <?php
+                        $currentDateTime = date('l, d F Y');
+                        echo $currentDateTime;
+                        ?>
+                    </div>
                 </div>
             </div>
-        </div>
-        @can('admin')
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="grid grid-cols-4 gap-5 my-5">
                     <div
