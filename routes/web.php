b229fd3ab4e7ad5ffbecfed067ce89822a43b39f
AskarKadir <askarkadir07@gmail.com>
2023-08-23 10:41:56 +0700

    routes secure

--- routes/web.php
+++ routes/web.php
@@ -21,30 +21,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', [DashboardController::class,''] {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware(['verified', 'auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::middleware('admin')->group(function () {
+        Route::patch('attendance/{attendance}/hadir', [AttendanceController::class, 'hadir'])->name('attendance.hadir');
+        Route::patch('attendance/{attendance}/absen', [AttendanceController::class, 'absen'])->name('attendance.absen');
+        Route::patch('attendance/{attendance}/sakit', [AttendanceController::class, 'sakit'])->name('attendance.sakit');
+        Route::patch('attendance/{attendance}/izin', [AttendanceController::class, 'izin'])->name('attendance.izin');
+        Route::get('/attendance/create', [AttendanceController::class, 'create'])->name('attendance.create');
+        Route::post('/attendance/store', [AttendanceController::class, 'store'])->name('attendance.store');
+        Route::get('/user', [UserController::class, 'index'])->name('user.index');
+        Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+        Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+        Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
+    });
 });
 
-Route::middleware('admin')->group(function () {
-    Route::patch('attendance/{attendance}/hadir', [AttendanceController::class, 'hadir'])->name('attendance.hadir');
-    Route::patch('attendance/{attendance}/absen', [AttendanceController::class, 'absen'])->name('attendance.absen');
-    Route::patch('attendance/{attendance}/sakit', [AttendanceController::class, 'sakit'])->name('attendance.sakit');
-    Route::patch('attendance/{attendance}/izin', [AttendanceController::class, 'izin'])->name('attendance.izin');
-    Route::get('/attendance/create', [AttendanceController::class, 'create'])->name('attendance.create');
-    Route::post('/attendance/store', [AttendanceController::class, 'store'])->name('attendance.store');
-    Route::get('/user', [UserController::class, 'index'])->name('user.index');
-    Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
-    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
-    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
-});
 
 require __DIR__ . '/auth.php';
